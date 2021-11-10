de17b8996e55d1fd4cfd930c01ae504418641b97
isaias dgr <isaiasd.garciar@gmail.com>
2021-11-09 20:30:45 -0600

    add ex 1,2,3,4

---
+++ Ex_01_02_03.py
@@ -0,0 +1,32 @@
+# explicacion de codigo.
+# Strings
+print("Hola \"3.14\"")
+print("Hola '3.14'")
+print('Hola "3.14"')
+# print('Hola #')
+print(3.14)
+print("\thola mundo! \n")
+print('c')
+# print('👽')
+
+
+# Numeros & Operaciones
+print("--------------------")
+print(2+3)
+print(2+4-1)
+print(2+4-1/2)
+print((2+4)-1/2)
+print(2+(4-1)/2)
+
+print(237%23)
+print("Operacion:", (2+4)-1/2)
+
+
+# Operaciones logicas
+print("--------------------")
+print(3>2)
+print(3<2)
+print(3>=2)
+print(3<=2)
+print(2>=2)
+print(2==2)
---
+++ Ex_04.py
@@ -0,0 +1,24 @@
+# Lenguaje Tipado Dinamico Duck typing)
+
+car = "tesla"
+print("modelo:", car)
+
+car = "vw"
+print("modelo:", car)
+
+resultado = 2+2
+print("res:", resultado)
+
+resultado = 2.2-3.12 # floatante 
+print("res:", resultado)
+
+resultado = 2.2 > 3.12 # booleno falso/verdadero 
+print("res:", resultado)
+
+# nueva_variable = 12
+# print("nueva", nueva_variable)
+
+nueva_variable = 23 # pregunta de entrevistas 
+# igual de asignacion =
+# igual de comparacion ==
+print("nueva", nueva_variable)
